56ba473759420094b3ee1b0cb8b4b039c2b59a95
Ilfar <ishmatov_i@mail.ru>
2022-03-22 16:26:28 +0300

    Add post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
+from operator import mod
 from django.db import models
+from matplotlib import image
+from matplotlib.pyplot import text
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    data = models.DateTimeField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='event_image/')
