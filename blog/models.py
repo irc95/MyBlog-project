5ddaff0f80beb42d31553f3e8147048496036061
Ilfar <ishmatov_i@mail.ru>
2022-03-24 16:10:18 +0300

    last commit

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,15 @@
 from operator import mod
 from django.db import models
-from matplotlib import image
-from matplotlib.pyplot import text
+
 
 class Post(models.Model):
     title = models.CharField(max_length=300)
     data = models.DateTimeField()
     text = models.TextField()
     image = models.ImageField(upload_to='event_image/')
+
+    def get_summary(self):
+        return self.text[:70]
+
+    def __str__(self):
+        return self.title
